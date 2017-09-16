4f8bb6bf98cfa3db1b04ff046a3bec4e4f1f2ff8
Andrei <andreikazmirovich1@gmail.com>
2017-09-16 17:13:06 +0300

    Now user can text '/next' and see what's next subject today

--- index.js
+++ index.js
@@ -1,20 +1,73 @@
-const TelegramBot = require('node-telegram-bot-api'),
-	Cron = require('cron').CronJob,
-	request = require('request'),
-	token = '388812803:AAHr_1dJQW_24_fVh5EPuIlxvIoWxmrCQRY';
+var TelegramBot = require('node-telegram-bot-api'),
+    request = require('request'),
+    jsdom = require('jsdom'),
+    { JSDOM } = jsdom;
+    token = '388812803:AAHr_1dJQW_24_fVh5EPuIlxvIoWxmrCQRY';
+
 
 var bot = new TelegramBot(token, {
-	polling: true,
+  polling: true,
 });
 
-const job = new Cron('* * * * * *', function() {
-	console.log('You will see this message every second');
-});
+bot.onText(/\/start/, (msg) => {
+  request('http://www.lp.edu.ua/rozklad-dlya-studentiv-zaochnykiv?group=%D0%9A%D0%9D-22%D0%B7&semestr=0', (error, response, body) => {
+    var dom = new JSDOM(body),
+        table = dom.window.document.querySelector("table.outer tbody");
 
-job.start();
+    var tableTrs = table.querySelectorAll('tr'),
+        days = [],
+        userID = msg.chat.id;
+    
 
-bot.on('message', (msg) => {
-	var id = msg.from.id;
-	bot.sendMessage(id, msg.text);
-	// console.log(msg);
+    var arr = [tableTrs[0]];
+
+    for(var i = 1; i < tableTrs.length; i++){
+      if(!!tableTrs[i].querySelector('.data')){
+        arr.splice(1,1);
+        days.push(arr);
+        arr = [];
+      }
+      arr.push(tableTrs[i]);
+    };
+
+
+    var timetable = ['8:30', '10:20', '12:10', '14:15', '16:00', '17:40', '19:20', '21:00'];
+    	date = new Date();
+    	date.setHours('21');
+    	date.setMinutes('20');
+    var curTime = `${date.getHours()}${date.getMinutes()}`,
+        curDate = `${date.getDate()}.${date.getMonth()+1 < 10 ? `0${date.getMonth()+1}` : date.getMonth()+1}.${date.getFullYear()}`;
+
+    days[5][0].textContent = '16.09.2017';
+    // console.log(days[0][0].textContent);
+
+    var message = '';
+    
+    for(var i = 0; i < days.length; i++){
+    	if(days[i][0].textContent === curDate){
+
+	        for(var j = 0; j < timetable.length; j++){
+	        	if(+timetable[j].replace(':', '') > +curTime){
+	        		days[i][j+1].children[0].remove();
+
+	        		var subject = days[i][j+1].children[0].textContent,
+	        			teacher = days[i][j+1].children[1].textContent,
+	        			building = days[i][j+1].children[2].textContent,
+	        			room = days[i][j+1].children[3].textContent;
+
+	        		// console.log(`${subject} - ${teacher} - ${building} - ${room}`);
+	        		message = `Наступна пара - <b>${subject}</b>\nВідбудеться о <b>${timetable[j]}</b> в аудиторії №<b>${room}</b>, <b>${building}</b>`;
+	        		break;
+	        	}
+	        }
+      	}
+    }
+    if(message.length != 0){
+    	bot.sendMessage(userID, message, {parse_mode : "HTML"});
+    }
+    else{
+    	bot.sendMessage(userID, 'Більше сьогодні пар немає!');
+    }
+
+  });
 });
