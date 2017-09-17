2300aa24e9d84118d747a93dbd898cd333edf03d
Andrei <andreikazmirovich1@gmail.com>
2017-09-17 23:45:04 +0300

    fixed bug with inconsistent schedule and added func to show full today schedule

--- index.js
+++ index.js
@@ -9,65 +9,98 @@ var bot = new TelegramBot(token, {
   polling: true,
 });
 
+var url = 'http://www.lp.edu.ua/rozklad-dlya-studentiv-zaochnykiv?group=%D0%9A%D0%9D-22%D0%B7&semestr=0',
+	timetable = ['8:30', '10:20', '12:10', '14:15', '16:00', '17:40', '19:20', '21:00'];
+
+var getArrOfSubjects = function (msg, body) {
+	var dom = new JSDOM(body),
+	    table = dom.window.document.querySelector("table.outer tbody");
+
+	var tableTrs = table.querySelectorAll('tr'),
+	days = [];
+	    
+	var arr = [tableTrs[0]];
+
+	for(var i = 1; i < tableTrs.length; i++){
+		if(!!tableTrs[i].querySelector('.data')){
+	        arr.splice(1,1);
+	        days.push(arr);
+	        arr = [];
+	    }
+	    arr.push(tableTrs[i]);
+	};
+
+	return days;
+};
+
 bot.onText(/\/next/, (msg) => {
-  request('http://www.lp.edu.ua/rozklad-dlya-studentiv-zaochnykiv?group=%D0%9A%D0%9D-22%D0%B7&semestr=0', (error, response, body) => {
-    var dom = new JSDOM(body),
-        table = dom.window.document.querySelector("table.outer tbody");
+	request(url, (error, response, body) => {
+	    var userID = msg.chat.id,
+	    	days = getArrOfSubjects(msg, body),
+	    	date = new Date();
+	    	date.setHours('14');
+	    	date.setMinutes('20');
+	    var curTime = `${date.getHours()}${date.getMinutes()}`,
+	        curDate = `${date.getDate()}.${date.getMonth()+1 < 10 ? `0${date.getMonth()+1}` : date.getMonth()+1}.${date.getFullYear()}`;
 
-    var tableTrs = table.querySelectorAll('tr'),
-        days = [],
-        userID = msg.chat.id;
-    
+	    days[5][0].textContent = '17.09.2017';
+	    // days[5][5].children[0].textContent = '6';
 
-    var arr = [tableTrs[0]];
+	    var message = '';
 
-    for(var i = 1; i < tableTrs.length; i++){
-      if(!!tableTrs[i].querySelector('.data')){
-        arr.splice(1,1);
-        days.push(arr);
-        arr = [];
-      }
-      arr.push(tableTrs[i]);
-    };
+	    for(var i = 0; i < days.length; i++){
+	    	if(days[i][0].textContent === curDate){
 
+		        for(var j = 0; j < timetable.length; j++){
+		        	if(+timetable[j].replace(':', '') > +curTime){
 
-    var timetable = ['8:30', '10:20', '12:10', '14:15', '16:00', '17:40', '19:20', '21:00'];
-    	date = new Date();
-    	date.setHours('21');
-    	date.setMinutes('20');
-    var curTime = `${date.getHours()}${date.getMinutes()}`,
-        curDate = `${date.getDate()}.${date.getMonth()+1 < 10 ? `0${date.getMonth()+1}` : date.getMonth()+1}.${date.getFullYear()}`;
+		        		var subject = days[i][j+1].children[1].textContent,
+		        			teacher = days[i][j+1].children[2].textContent,
+		        			building = days[i][j+1].children[3].textContent,
+		        			room = days[i][j+1].children[4].textContent;
 
-    days[5][0].textContent = '16.09.2017';
-    // console.log(days[0][0].textContent);
+		        		message = `Наступна пара - <b>${subject}</b>\nВідбудеться о <b>${timetable[days[i][j+1].children[0].textContent - 1]}</b> в аудиторії №<b>${room}</b>, <b>${building}</b>`;
+		        		break;
+		        	}
+		        }
+	      	}
+	    }
+	    if(message.length != 0){
+	    	bot.sendMessage(userID, message, {parse_mode : "HTML"});
+	    }
+	    else{
+	    	bot.sendMessage(userID, 'Більше сьогодні пар немає!');
+	    }
 
-    var message = '';
+	});
+});
 
-    for(var i = 0; i < days.length; i++){
-    	if(days[i][0].textContent === curDate){
+bot.onText(/\/today/, (msg) => {
+	request(url, (error, response, body) => {
+		var userID = msg.chat.id,
+			days = getArrOfSubjects(msg, body),
+			date = new Date();
+	    	date.setHours('11');
+	    	date.setMinutes('20');
+	    var curTime = `${date.getHours()}${date.getMinutes()}`,
+	        curDate = `${date.getDate()}.${date.getMonth()+1 < 10 ? `0${date.getMonth()+1}` : date.getMonth()+1}.${date.getFullYear()}`;
 
-	        for(var j = 0; j < timetable.length; j++){
-	        	if(+timetable[j].replace(':', '') > +curTime){
-	        		days[i][j+1].children[0].remove();
+	    days[5][0].textContent = '17.09.2017';
+	    // days[5][5].children[0].textContent = '6';
 
-	        		var subject = days[i][j+1].children[0].textContent,
-	        			teacher = days[i][j+1].children[1].textContent,
-	        			building = days[i][j+1].children[2].textContent,
-	        			room = days[i][j+1].children[3].textContent;
+	    var message = '';
 
-	        		// console.log(`${subject} - ${teacher} - ${building} - ${room}`);
-	        		message = `Наступна пара - <b>${subject}</b>\nВідбудеться о <b>${timetable[j]}</b> в аудиторії №<b>${room}</b>, <b>${building}</b>`;
-	        		break;
-	        	}
-	        }
-      	}
-    }
-    if(message.length != 0){
-    	bot.sendMessage(userID, message, {parse_mode : "HTML"});
-    }
-    else{
-    	bot.sendMessage(userID, 'Більше сьогодні пар немає!');
-    }
-
-  });
+	    for(var i = 0; i < days.length; i++){
+	    	if(days[i][0].textContent === curDate){
+	    		/*days[i].forEach(function(item, index) {
+	    			console.log(item.children[0].textContent);
+	    			// message += `${index} - <b>${item.children[0].textContent}</b>`
+	    		});*/
+	    		for(var j = 1; j < days[i].length; j++){
+	    			message += `${days[i][j].children[0].textContent} - <b>${days[i][j].children[1].textContent}</b>\n`;
+	    		}
+	      	}
+	    }
+	    bot.sendMessage(userID, message, {parse_mode : "HTML"});
+	});
 });
